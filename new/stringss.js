9d05f97d41e04461cc63d4b760baf53f5a4028db
Ayush yadav <ayushyadav8900@gmail.com>
2025-04-04 16:37:55 +0000

    new

---
+++ new/stringss.js
@@ -0,0 +1,11 @@
+const name = "Ayush"
+const gameName = new String("Ayushya")
+// console.log(gameName)
+
+//now using at proto of sting
+const sentence ="The quick brown fox jumps over the lazy dog."
+
+const index = 20
+//here need to^ give index number to find which word was there
+
+console.log(`here is the number ${index}and now convert it into and find word ${sentence.at(index)}`)
---
+++ new/test-1.js
@@ -0,0 +1,5 @@
+let name = "Ayush"
+let repoCount = 50
+
+// console.log( name +  repoCount + " Value")
+console.log(`Hello My Name is ${name} And My RepoCount is ${repoCount}`)
