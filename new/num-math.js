751d3aa586f433d7e6a19658e3d2b9c4a6e21445
Ayush yadav <ayushyadav8900@gmail.com>
2025-04-13 04:37:22 +0000

    maths

---
+++ new/num-math.js
@@ -0,0 +1,39 @@
+const score = 90
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+
+const fex = 1000
+// console.log(fex.toFixed(2));
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// *************************************maths*************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.637));
+// console.log(Math.ceil(4.637));
+// console.log(Math.floor(4.637));
+// console.log(Math.min(4,2,5,7,1));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+min = 10 
+max = 20
+// important++++++++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math.floor(Math.random() * (max - min + 1 )) + min )
+
+
+
+
