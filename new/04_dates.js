a6434ee860db7985ed17dcbc8555c2e468d026da
Ayush yadav <ayushyadav8900@gmail.com>
2025-04-13 05:10:17 +0000

    dates-and-time

---
+++ new/04_dates.js
@@ -0,0 +1,23 @@
+// Dates
+
+// let MyDate = new Date()
+// console.log(MyDate);
+// console.log(MyDate.toString());
+// console.log(MyDate.toISOString());
+// console.log(MyDate.toJSON());
+// console.log(MyDate.toLocaleDateString());
+// console.log(MyDate.toLocaleString());
+// console.log(MyDate.toLocaleTimeString());
+// console.log(MyDate.toUTCString());
+
+let NewDate = new Date();
+// console.log(NewDate);
+// console.log(NewDate.getDate());
+
+
+NewDate.toLocaleString('default',{
+    weekday: "long"
+})
+console.log(NewDate);
+
+
