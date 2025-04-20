34d645a2bf628fe3846f9124dda23849856409df
Ayush yadav <ayushyadav8900@gmail.com>
2025-04-20 05:17:00 +0000

    function obj array

--- basics-section-3/01_function.js
+++ basics-section-3/01_function.js
@@ -27,5 +27,48 @@ function loginUserMessage(username){
 }
 loginUserMessage("Ayush")
 //isme ek catch hai ye value print hogi hi nh bcuz hume usko return kiya vo code me to retrun hogyi hogi par console pe nh dikhaegi
-console.log(loginUserMessage("Ayush"));
-console.log(loginUserMessage());//interview Question agr hum agrument pass na kare to ky hoga ye null nh dega ye undefine dega remember
+// console.log(loginUserMessage("Ayush"));
+// console.log(loginUserMessage());//interview Question agr hum agrument pass na kare to ky hoga ye null nh dega ye undefine dega remember
+
+
+
+
+function calculateCartPrice(...num1){ // ye ten dot... ko hi rest operator or spread operator bolte hai bss inki use case me diffrence hai ye karta jitne sari values hai sabko ek bundle me pack kar deta hai fir hume deta hai..
+    return num1
+}
+
+// console.log(calculateCartPrice(100,200,300));
+
+function calculateCartPrice(val1,val2,...num1){//interview question in sabme se konsi value kisko milegi number wise hi milega baki jo bachega vo sab jisme rest hoga simpel
+    return num1
+}
+
+// console.log(calculateCartPrice(100,200,300));
+
+
+const user = {
+    username:"Ayush",
+    price:99,
+}
+
+function handleObject (anyobject){
+    console.log(`student name is ${anyobject.username} and how much price is ${ anyobject.price}`);
+    
+}
+// handleObject(user)
+
+// handleObject({
+//     username:"Ramesh",
+//     price:700,
+// })
+
+
+const myNewArray = [100,200,300,400]
+
+function returnSecondValue(getArray){
+    return getArray[3]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100,200,300,400]));
+
